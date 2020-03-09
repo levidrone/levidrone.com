ba5227874702e5c165c312e988fa345f780a26c3
S. Yakupov <s.yakupov@noviga.com>
2020-03-09 23:25:50 +0300

    Add content

--- scripts/svg2favicon.sh
+++ scripts/svg2favicon.sh
@@ -19,3 +19,9 @@ icotool -o ../static/img/favicon.ico -c $PNGLIST
 
 # Clean up
 rm -rf $PNGLIST
+
+# PNG icons
+for SIZE in 16 32 180 192 512
+do
+  inkscape -z ../img/levidrone-icon.svg --export-png=../static/img/icon${SIZE}.png --export-width=$SIZE
+done
